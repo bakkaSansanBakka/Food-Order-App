b7b416b8c985ce25eccfd48dd6aaaceb0fdb1f77
bakkaSansanBakka <polina.potapeyko@mail.ru>
2021-08-27 23:19:39 +0300

    added meals component

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,16 @@
+import React from "react";
+import AvailableMeals from "./AvailableMeals";
+import MealsSummary from "./MealsSummary";
+
+const Meals = () => {
+  return (
+    <>
+      <MealsSummary />
+      <main>
+        <AvailableMeals />
+      </main>
+    </>
+  );
+};
+
+export default Meals;
