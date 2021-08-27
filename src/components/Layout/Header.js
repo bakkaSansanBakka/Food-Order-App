a421800927354bc5428b932d9979af064191b83a
bakkaSansanBakka <polina.potapeyko@mail.ru>
2021-08-27 18:08:34 +0300

    added header component with toolbar and main image

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import mealsImage from "../../assets/Food_header.jpg";
+import classes from "./Header.module.css";
+
+const Header = (props) => {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <button>Cart</button>
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="A table full of delicious meals!" />
+      </div>
+    </>
+  );
+};
+
+export default Header;
